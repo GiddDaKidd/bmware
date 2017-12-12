096d3b921eccedf1aa89ccbe7d8ba342b56d5d21
Gideon <mwaniki59@gmail.com>
2017-12-12 10:10:08 -0600

    put ext on styleshhet link

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.compile = false
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( application.css )
+Rails.application.config.assets.precompile += %w( application.css )
